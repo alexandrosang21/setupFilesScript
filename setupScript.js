223f53ea624d5036926969ff6980271e346e38fd
alexandrosang <alexandrosang21@gmail.com>
2020-11-19 01:50:43 +0200

    mkdirSync and writeFileSync

---
+++ setupScript.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const folderName = process.argv[2] || 'ProjectFolder';
+
+const htmlDoc = `<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+    <script src="app.js"></script>
+</body>
+</html>`;
+
+fs.mkdirSync(`${folderName}`);
+fs.writeFileSync(`${folderName}/index.html`, `${htmlDoc}`);
+fs.writeFileSync(`${folderName}/app.css`, '');
+fs.writeFileSync(`${folderName}/app.js`, '');
